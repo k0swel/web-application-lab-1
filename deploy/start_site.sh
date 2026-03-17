d723fa96b5fc3c203591ee7fd7492b527ff6c569
k0swel <romanleonenko32@gmail.com>
2026-03-17 04:11:42 +0300

    Delete deploy directory

--- deploy/start_site.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-site_dir=/home/debian/frontend
-executable_file=/app/app.py
-
-source $site_dir/.venv/bin/activate
-cd $site_dir/app
-. $site_dir/.env
-gunicorn --workers 4 -b 0.0.0.0:80 'app:app'
